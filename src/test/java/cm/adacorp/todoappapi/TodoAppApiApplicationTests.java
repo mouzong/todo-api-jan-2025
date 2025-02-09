95188e8d5df588e20f91b5486e986880c03fba98
Andreas MOUZONG <andreasmouzong@gmail.com>
2025-02-09 15:48:25 +0100

    Initial commit:  Bootstrapping the project with swagger documentation

---
+++ src/test/java/cm/adacorp/todoappapi/TodoAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.adacorp.todoappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
